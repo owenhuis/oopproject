00e6819de5496f2554b2f2d5e2872185874b49a7
owenhuis <143698007+owenhuis@users.noreply.github.com>
2025-02-14 09:36:17 +0100

    pokemonBattleSim opdracht 1 klaar

---
+++ pokemonBattleSim/pokemonBattleSim/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemonBattleSim
+{
+    internal class Program
+    {
+
+        
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("je hebt een charmander");
+
+            while (true)
+            {
+                Console.WriteLine("wat voor naam wil je geven aan charmander");
+                string naam = Console.ReadLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(naam);
+                }
+                Console.WriteLine("wil je een andere naam? (ja/nee)");
+                string keuze = Console.ReadLine();
+                if (keuze == "nee")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
